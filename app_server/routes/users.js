2cc90f14b46cbf4ed912d009f7abf4707b87f549
fatimahanif <fatimahanif303@gmail.com>
2023-01-06 20:52:01 +0500

    token generated on login and signup

--- app_server/routes/users.js
+++ app_server/routes/users.js
@@ -1,6 +1,12 @@
 var express = require("express");
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
 var router = express.Router();
+
+// secret key
+dotenv.config();
+const secret_key = process.env.secret_key;
 
 /* GET users listing. */
 router.get("/", (req, res, next) => {
@@ -9,19 +15,31 @@ router.get("/", (req, res, next) => {
 
 // creating a new user
 router.post("/signup", (req, res, next) => {
-  let password = req.body.password;
-  let hashedPassword = User.hashPassword(password);
-  console.log(hashedPassword);
   User.create({
     name: req.body.name,
     email: req.body.email,
-    password: hashedPassword,
+    password: User.hashPassword(req.body.password),
   }).then(
     (user) => {
       console.log("User created with id", user._id);
+      let token;
+      try {
+        token = jwt.sign({ _id: user._id, email: user.email }, secret_key, {
+          expiresIn: "5h",
+        });
+      } catch (err) {
+        console.log("Error in login");
+        res.statusCode = 400;
+        res.json("Please try again");
+      }
       res.statusCode = 200;
-      res.setHeader("Content-Type", "application/json");
-      res.json(user);
+      res.json({
+        success: true,
+        data: {
+          user: user,
+          token: token,
+        },
+      });
     },
     (err) => {
       console.log("Error in creating new user");
@@ -32,22 +50,53 @@ router.post("/signup", (req, res, next) => {
 });
 
 router.post("/login", (req, res, next) => {
-  User.findOne({ email: req.body.email }).then(
-    (user) => {
-      if (user == null) {
-        res.statusCode = 404;
-        res.send("User not found");
-      } else if (!User.comparePassword(req.body.password, user.password)) {
-        res.statusCode = 401;
-        res.send("Incorrect email or password");
+  User.findOne({ email: req.body.email })
+    .then(
+      (user) => {
+        if (user == null) {
+          res.statusCode = 404;
+          res.send("User not found");
+        } else if (!User.comparePassword(req.body.password, user.password)) {
+          res.statusCode = 401;
+          res.send("Incorrect email or password");
+        }
+        // console.log("User found");
+        // res.json(user);
+        return user;
+      },
+      (err) => {
+        console.log("Error in login");
+        res.statusCode = 400;
+        res.json("Please try again");
       }
-      console.log("User found");
-      res.json(user);
-    },
-    (err) => {
-      console.log("Error in login");
-    }
-  );
+    )
+    .then(
+      (user) => {
+        let token;
+        try {
+          token = jwt.sign({ _id: user._id, email: user.email }, secret_key, {
+            expiresIn: "5h",
+          });
+        } catch (err) {
+          console.log("Error in login");
+          res.statusCode = 400;
+          res.json("Please try again");
+        }
+        res.statusCode = 200;
+        res.json({
+          success: true,
+          data: {
+            user: user,
+            token: token,
+          },
+        });
+      },
+      (err) => {
+        console.log("Error in login");
+        res.statusCode = 400;
+        res.json("Please try again");
+      }
+    );
 });
 
 module.exports = router;
